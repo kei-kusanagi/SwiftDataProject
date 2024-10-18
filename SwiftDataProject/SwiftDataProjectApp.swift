f4ec21b03b90985653ec4391551c8d86ac051e80
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-10-18 12:53:32 -0600

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Juan Carlos Robledo Morales on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
